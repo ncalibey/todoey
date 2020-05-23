5885a648c3bd6aacff4dc0d934d7eb0a8b1b44d3
Nick Calibey <nick.calibey@gmail.com>
2020-05-23 15:33:00 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Todoey
+===
+
+Todoey is a simple todo app build using Flutter. It is the final project of [Angela Yu's Flutter course on Udemy](https://www.udemy.com/course/flutter-bootcamp-with-dart/learn/). The app uses Provider to do state management.
---
+++ lib/screens/add_task_screen.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:todoey/models/task_data.dart';
+
+class AddTaskScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    String taskName;
+
+    return Container(
+      color: Color(0xff757575),
+      child: Container(
+        padding: EdgeInsets.all(20.0),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(20.0),
+            topRight: Radius.circular(20.0),
+          ),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Text(
+              'Add Task',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Colors.lightBlueAccent,
+                fontSize: 30.0,
+              ),
+            ),
+            TextField(
+              autofocus: true,
+              textAlign: TextAlign.center,
+              onChanged: (value) {
+                taskName = value;
+              },
+            ),
+            FlatButton(
+              child: Text(
+                'Add',
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+              color: Colors.lightBlueAccent,
+              onPressed: () {
+                Provider.of<TaskData>(context, listen: false).addTask(taskName);
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
